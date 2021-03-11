41f1360b786892b99cd68cee95fb3bc258386179
Tim Burks <timburks@google.com>
2021-03-10 19:46:26 -0800

    cleanup/refactor Discovery and GoogleAPIs importers (#21)

--- apps/import-discovery/bin/import-discovery.dart
+++ apps/import-discovery/bin/import-discovery.dart
@@ -13,160 +13,77 @@
 // limitations under the License.
 
 import 'dart:convert';
+
 import 'package:archive/archive.dart';
 import 'package:http/http.dart' as http;
+import 'package:importer/importer.dart';
 import 'package:registry/registry.dart' as rpc;
 
-class DiscoveryApiListing {
-  String id;
-  String version;
-  String title;
-  String discoveryUrl;
-  DiscoveryApiListing({this.id, this.version, this.title, this.discoveryUrl});
-}
-
-String idForTitle(String title) {
-  var id = title.toLowerCase();
-  // remove the google prefix because it is applied inconsistently
-  if (id.startsWith("google ")) {
-    id = id.substring("google ".length);
-  }
-  // remove anything in parentheses
-  if (id.contains("(")) {
-    final start = id.indexOf("(");
-    final end = id.indexOf(")");
-    id = id.substring(0, start - 1) + id.substring(end + 1);
-  }
-  // remove some superfluous words
-  if (id.contains(" api")) {
-    id = id.replaceAll(" api", "");
-  }
-  if (id.contains(" v2")) {
-    id = id.replaceAll(" v2", "");
-  }
-  if (id.contains(" v3")) {
-    id = id.replaceAll(" v3", "");
-  }
-  // make it printable
-  id = id.replaceAll("&", "and");
-  id = id.replaceAll("/", "");
-  id = id.replaceAll(" ", "-");
-  // consistently prefix with "google-"
-  id = "google-" + id;
-  return id;
-}
-
-String titleForTitle(String title) {
-  if (!title.startsWith("Google ")) {
-    title = "Google " + title;
-  }
-  return title;
-}
-
-Future<List<dynamic>> fetchApiListings() {
-  return http
-      .get(Uri.parse('https://www.googleapis.com/discovery/v1/apis'))
-      .then((response) {
-    Map<String, dynamic> discoveryList = jsonDecode(response.body);
-    return discoveryList["items"];
-  });
-}
+final projectDescription = "APIs from a variety of sources";
+final projectDisplayName = "Motley APIs";
+final projectName = "projects/motley";
+final source = "discovery";
 
 void main(List<String> arguments) async {
   final channel = rpc.createClientChannel();
-  final client = rpc.RegistryClient(channel);
+  final client = rpc.RegistryClient(channel, options: rpc.callOptions());
 
-  // does the project exist?
-  try {
-    var request = rpc.GetProjectRequest()..name = "projects/motley";
-    await client.getProject(request, options: rpc.callOptions());
-  } catch (error) {
-    var project = rpc.Project()
-      ..displayName = "Motley"
-      ..description = "APIs from a variety of sources";
-    var request = rpc.CreateProjectRequest()
-      ..projectId = "motley"
-      ..project = project;
-    await client.createProject(request, options: rpc.callOptions());
-  }
+  client.ensureProjectExists(rpc.Project()
+    ..name = projectName
+    ..displayName = projectDisplayName
+    ..description = projectDescription);
 
-  final listings = await fetchApiListings();
-  for (var item in listings) {
+  for (var item in await fetchApiListings()) {
+    // get basic API attributes from the Discovery Service list
     var dict = item as Map<String, dynamic>;
-    var id = idForTitle(dict["title"] as String);
-    var version = dict["version"] as String;
-    var title = titleForTitle(dict["title"] as String);
+    var title = dict["title"];
+    var apiId = GoogleApis.idForTitle(title);
+    var apiTitle = GoogleApis.titleForTitle(title);
+    var versionId = dict["version"] as String;
+
+    // read the discovery doc
     var discoveryUrl = dict["discoveryRestUrl"] as String;
-    print(id + " " + version);
-    // does the api exist?
-    try {
-      var request = rpc.GetApiRequest()..name = "projects/motley/apis/" + id;
-      await client.getApi(request, options: rpc.callOptions());
-    } catch (error) {
-      var api = rpc.Api()..displayName = title;
-      api.labels["created-from"] = "discovery";
-      var request = rpc.CreateApiRequest()
-        ..parent = "projects/motley"
-        ..apiId = id
-        ..api = api;
-      await client.createApi(request, options: rpc.callOptions());
-    }
-    // does the version exist?
-    try {
-      var request = rpc.GetApiVersionRequest()
-        ..name = "projects/motley/apis/" + id + "/versions/" + version;
-      await client.getApiVersion(request, options: rpc.callOptions());
-    } catch (error) {
-      var apiVersion = rpc.ApiVersion()..displayName = version;
-      apiVersion.labels["created-from"] = "discovery";
-      var request = rpc.CreateApiVersionRequest()
-        ..parent = "projects/motley/apis/" + id
-        ..apiVersionId = version
-        ..apiVersion = apiVersion;
-      await client.createApiVersion(request, options: rpc.callOptions());
-    }
-    // does the spec exist?
-    try {
-      var request = rpc.GetApiSpecRequest()
-        ..name = "projects/motley/apis/" +
-            id +
-            "/versions/" +
-            version +
-            "/specs/discovery.json";
-      await client.getApiSpec(request, options: rpc.callOptions());
-    } catch (error) {
-      var doc = await http.get(Uri.parse(discoveryUrl));
+    var doc = await http.get(Uri.parse(discoveryUrl));
+    Map<String, dynamic> discoveryDoc = jsonDecode(doc.body);
+    var description = discoveryDoc["description"] ?? "";
+
+    print("uploading $apiId $versionId");
+
+    final apiName = projectName + "/apis/" + apiId;
+    var api = rpc.Api()
+      ..name = apiName
+      ..displayName = apiTitle
+      ..description = description;
+    api.labels["created-from"] = source;
+    api.labels["google-title"] = title;
+    await client.ensureApiExists(api);
+
+    final versionName = apiName + "/versions/" + versionId;
+    var version = rpc.ApiVersion()
+      ..name = versionName
+      ..displayName = versionId;
+    version.labels["created-from"] = source;
+    await client.ensureApiVersionExists(version);
+
+    final specName = versionName + "/specs/discovery.json";
+    if (!await client.apiSpecExists(specName)) {
       try {
-        Map<String, dynamic> discoveryDoc = jsonDecode(doc.body);
         var contents = GZipEncoder().encode(doc.bodyBytes);
         var apiSpec = rpc.ApiSpec()
           ..filename = "discovery.json"
           ..contents = contents
           ..sourceUri = discoveryUrl
           ..mimeType = "application/x.discovery+gzip";
-        apiSpec.labels["created-from"] = "discovery";
-        var revision = discoveryDoc["revision"] as String;
+        apiSpec.labels["created-from"] = source;
+        var revision = discoveryDoc["revision"];
         if (revision != null) {
           apiSpec.labels["revision-date"] = revision;
         }
-
         var request = rpc.CreateApiSpecRequest()
-          ..parent = "projects/motley/apis/" + id + "/versions/" + version
-          ..apiSpecId = "discovery.json"
+          ..parent = versionName
+          ..apiSpecId = specName.split("/").last
           ..apiSpec = apiSpec;
-        await client.createApiSpec(request, options: rpc.callOptions());
-        // use the spec to update the API description
-        var description = discoveryDoc["description"] as String;
-        var title = discoveryDoc["title"] as String;
-        var getRequest = rpc.GetApiRequest()
-          ..name = "projects/motley/apis/" + id;
-        var api = await client.getApi(getRequest, options: rpc.callOptions());
-        api.description = description ?? "";
-        api.labels["google-title"] = title ?? "";
-        var updateRequest = rpc.UpdateApiRequest()
-          ..api = api
-          ..updateMask = rpc.FieldMask(paths: ["description", "labels"]);
-        await client.updateApi(updateRequest, options: rpc.callOptions());
+        await client.createApiSpec(request);
       } catch (error) {
         print("$error");
         print(discoveryUrl);
@@ -176,4 +93,13 @@ void main(List<String> arguments) async {
   }
 
   await channel.shutdown();
+}
+
+Future<List<dynamic>> fetchApiListings() {
+  return http
+      .get(Uri.parse('https://www.googleapis.com/discovery/v1/apis'))
+      .then((response) {
+    Map<String, dynamic> discoveryList = jsonDecode(response.body);
+    return discoveryList["items"];
+  });
 }
