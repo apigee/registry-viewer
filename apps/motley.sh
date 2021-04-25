1d98f59d84eae0567e438c006ecabec494e3ff57
Tim Burks <timburks@google.com>
2021-04-25 14:53:58 -0700

    remove standalone tools that were merged into the reg tool.

--- apps/motley.sh
+++ apps/motley.sh
@@ -10,8 +10,8 @@ apg registry create-project \
 
 reg import common-protos --project projects/motley --path ~/Desktop/api-common-protos
 reg import googleapis --project projects/motley --path ~/Desktop/googleapis
-reg compute-summary --project projects/motley
-reg label-apis --project projects/motley
+reg compute summary --project projects/motley
+reg label apis --project projects/motley
 
 registry compute references projects/motley/apis/-/versions/-/specs/-
 registry compute complexity projects/motley/apis/-/versions/-/specs/-
