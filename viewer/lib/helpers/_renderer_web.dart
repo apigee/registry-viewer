68662a285923c6201e6cf7d7788ecc9ce1f76e2c
Tim Burks <timburks@google.com>
2022-10-11 20:05:08 -0700

    Use the universal_html package to fix a dart analyze warning.
    "Avoid using web-only libraries outside Flutter web plugin packages"
    
    https://stackoverflow.com/a/60646794/35844

--- viewer/lib/helpers/_renderer_web.dart
+++ viewer/lib/helpers/_renderer_web.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:html' as html;
+import 'package:universal_html/html.dart' as html;
 
 String rendererServiceAddress() {
   return html
