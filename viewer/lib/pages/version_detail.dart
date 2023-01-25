553427eea262299e8a36b13e35460e9b38a8a4e4
Tim Burks <timburks@google.com>
2023-01-25 07:37:56 -0800

    Use a simple "media query" to adjust content for narrow screens. (#63)

--- viewer/lib/pages/version_detail.dart
+++ viewer/lib/pages/version_detail.dart
@@ -23,6 +23,7 @@ import '../components/artifact_detail.dart';
 import '../components/bottom_bar.dart';
 import '../components/home_button.dart';
 import '../components/split_view.dart';
+import '../helpers/media.dart';
 import '../helpers/tab_decoration.dart';
 import '../helpers/title.dart';
 
@@ -70,20 +71,25 @@ class VersionDetailPage extends StatelessWidget {
                 child: TabBarView(
                   children: [
                     VersionDetailCard(editable: true),
-                    CustomSplitView(
-                      viewMode: SplitViewMode.Horizontal,
-                      initialWeight: 0.33,
-                      view1: SpecListCard(),
-                      view2: SpecDetailCard(selflink: true, editable: true),
-                    ),
-                    CustomSplitView(
-                      viewMode: SplitViewMode.Horizontal,
-                      view1: ArtifactListCard(SelectionProvider.version),
-                      view2: ArtifactDetailCard(
-                        selflink: true,
-                        editable: true,
-                      ),
-                    ),
+                    narrow(context)
+                        ? SpecListCard()
+                        : CustomSplitView(
+                            viewMode: SplitViewMode.Horizontal,
+                            initialWeight: 0.33,
+                            view1: SpecListCard(),
+                            view2:
+                                SpecDetailCard(selflink: true, editable: true),
+                          ),
+                    narrow(context)
+                        ? ArtifactListCard(SelectionProvider.version)
+                        : CustomSplitView(
+                            viewMode: SplitViewMode.Horizontal,
+                            view1: ArtifactListCard(SelectionProvider.version),
+                            view2: ArtifactDetailCard(
+                              selflink: true,
+                              editable: true,
+                            ),
+                          ),
                   ],
                 ),
               ),
